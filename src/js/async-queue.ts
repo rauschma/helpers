cbd07230700dec417aaa12b10a53b573e9440d73
Axel Rauschmayer <axel@rauschma.de>
2022-10-10 02:51:39 +0200

    Fix bug in AsyncQueue

--- src/js/async-queue.ts
+++ src/js/async-queue.ts
@@ -1,19 +1,24 @@
 import { assertNonNullable, assertTrue } from '../ts/types.js';
 import { ArrayQueue } from './lang.js';
+import { createPromiseWithSettlers, PromiseWithSettlers } from './promise.js';
 
 /**
  * This class is a queue that works asynchronously:
- * - Enqueuing via `.enqueue()` is manual and synchronous (non-blocking) and can be done
- *   multiple times.
- *   - Rationale: Needed so that we can turn a callback-based (push) API into an
- *     asynchronous-iteration-based (pull) API.
- * - Dequeuing is done via async iteration, which invokes method `.next()`. This operation
- *   blocks asynchronously until data is enqueued.
+ * - Enqueuing via `.enqueue()` is manual and synchronous (non-blocking)
+ *   and can be done multiple times.
+ *   - Rationale: Needed so that we can turn a callback-based (push) API
+ *     into an asynchronous-iteration-based (pull) API.
+ * - Dequeuing is done via async iteration, which invokes method `.next()`.
+ *   This operation blocks asynchronously until data is enqueued.
  */
 export class AsyncQueue<T> {
   #enqueuedValues = new ArrayQueue<T>();
-  /** Non-null if there are no enqueued values */
-  #pendingValue: null | Settler<IteratorResult<T>> = null;
+  /**
+   * If no values are enqueued and a value is dequeued, this Promise is
+   * used to notify the dequeuer when a value is available. In other words:
+   * If this field is non-null, there are no enqeued values.
+   */
+  #pendingValue: null | PromiseWithSettlers<IteratorResult<T>> = null;
   #closed = false;
 
   [Symbol.asyncIterator]() {
@@ -22,16 +27,16 @@ export class AsyncQueue<T> {
 
   /**
    * Enqueuing is done manually.
-   * 
-   * This method is non-blocking. It can be called as often as you want and will buffer if
-   * more is enqeued than dequeued.
+   *
+   * This method is non-blocking. It can be called as often as you want and
+   * will buffer if more is enqeued than dequeued.
    */
   enqueue(value: T): this {
     if (this.#closed) {
       throw new Error('Closed');
     }
     if (this.#pendingValue) {
-      assertTrue(this.#enqueuedValues.length > 0);
+      assertTrue(this.#enqueuedValues.length === 0);
       this.#pendingValue.resolve({done: false, value});
       this.#pendingValue = null;
     } else {
@@ -42,9 +47,9 @@ export class AsyncQueue<T> {
 
   /**
    * Dequeuing is done via async iteration.
-   * 
-   * This method is blocking. That is, it must not be called again before its result has
-   * settled.
+   *
+   * This method is blocking. That is, it must not be called again before
+   * its result has settled.
    */
   next(): Promise<IteratorResult<T>> {
     if (this.#pendingValue) {
@@ -59,37 +64,21 @@ export class AsyncQueue<T> {
     if (this.#closed) {
       return Promise.resolve({done: true, value: undefined});
     }
-    // Wait for new values to be enqueued
-    const {promise, resolve, reject} = createPromise<IteratorResult<T>>();
-    this.#pendingValue = {resolve, reject};
-    return promise;
+    // Wait for new enqueued values or closing
+    const promiseWithSettlers = createPromiseWithSettlers<IteratorResult<T>>();
+    this.#pendingValue = promiseWithSettlers;
+    return promiseWithSettlers.promise;
   }
 
   close(): this {
     if (this.#closed) return this;
     this.#closed = true;
     if (this.#pendingValue) {
-      assertTrue(this.#enqueuedValues.length > 0);
+      assertTrue(this.#enqueuedValues.length === 0);
       this.#pendingValue.resolve({done: true, value: undefined});
       this.#pendingValue = null;
     }
+    // If values are enqueued, we allow them to be read
     return this;
   }
-}
-
-interface Settler<T> {
-  resolve(result: T): void;
-  reject(err: any): void;
-}
-
-function createPromise<T>() {
-  let resolve!: (result: T) => void;
-  let reject!: (error: any) => void;
-  const promise = new Promise<T>(
-    (res, rej) => {
-      // Executed synchronously
-      resolve = res;
-      reject = rej;
-    });
-  return {promise, resolve, reject};
 }
--- src/js/promise.ts
+++ src/js/promise.ts
@@ -1,9 +1,15 @@
-export function createPromise<T>() {
+export interface PromiseWithSettlers<T> {
+  promise: Promise<T>;
+  resolve: (result: T) => void;
+  reject: (error: any) => void;
+}
+
+export function createPromiseWithSettlers<T>(): PromiseWithSettlers<T> {
   let resolve!: (result: T) => void;
   let reject!: (error: any) => void;
   const promise = new Promise<T>(
     (res, rej) => {
-      // Executed synchronously
+      // Executed synchronously!
       resolve = res;
       reject = rej;
     });
