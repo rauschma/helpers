fc2c9b5008a45bfdce99bb25a8dfca2dbcbb57bb
Axel Rauschmayer <axel@rauschma.de>
2024-03-03 13:32:03 +0100

    Rename text-ink to text-style

--- src/nodejs/text-style_test.ts
+++ src/nodejs/text-style_test.ts
@@ -1,24 +1,24 @@
 import assert from 'node:assert/strict';
-import { ink } from './text-ink.js';
+import { style } from './text-style.js';
 
-test('ink as template tag', () => {
+test('style as template tag', () => {
   assert.equal(
-    ink.Underline.FgGreen`underlined green`,
+    style.Underline.FgGreen`underlined green`,
     '\x1B[4;32m' + 'underlined green' + '\x1B[0m'
   );
   assert.equal(
-    ink.FgColorCode(51)`turquoise`,
+    style.FgColorCode(51)`turquoise`,
     '\x1B[38;5;51m' + 'turquoise' + '\x1B[0m'
   );
 });
 
-test('ink as function', () => {
+test('style as function', () => {
   assert.equal(
-    ink.Bold('bold'),
+    style.Bold('bold'),
     '\x1B[1m' + 'bold' + '\x1B[0m'
   );
   assert.equal(
-    ink.FgColorCode(51)('turquoise'),
+    style.FgColorCode(51)('turquoise'),
     '\x1B[38;5;51m' + 'turquoise' + '\x1B[0m'
   );
 });
