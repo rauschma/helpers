b551c78d6e87aadac84c71bfdd8492c5d8f7bef6
Axel Rauschmayer <axel@rauschma.de>
2024-03-21 17:56:28 +0100

    Improve comments

--- src/lang/regexp.ts
+++ src/lang/regexp.ts
@@ -1,7 +1,9 @@
 const {raw} = String;
 
 export enum MatchMode {
+  /** No gaps between matches */
   AtLastIndex = 'AtLastIndex',
+  /** There can be gaps between matches */
   AtLastIndexOrLater = 'AtLastIndexOrLater',
 }
 
@@ -9,15 +11,15 @@ export function setLastIndex(matchMode: MatchMode, regExp: RegExp, lastIndex: nu
   switch (matchMode) {
     case MatchMode.AtLastIndex:
       if (!regExp.sticky) {
-        throw new TypeError(`If flag /y is not set, matching ${matchMode} won’t work: ${regExp}`);
+        throw new TypeError(`Flag /y must be set for matching ${matchMode}: ${regExp}`);
       }
       break;
     case MatchMode.AtLastIndexOrLater:
       if (!regExp.global) {
-        throw new TypeError(`If flag /g is not set, matching ${matchMode} won’t work: ${regExp}`);
+        throw new TypeError(`Flag /g must be set for matching ${matchMode}: ${regExp}`);
       }
       if (regExp.sticky) {
-        throw new TypeError(`If flag /y is set, matching ${matchMode} won’t work: ${regExp}`);
+        throw new TypeError(`Flag /y must be set for matching ${matchMode}: ${regExp}`);
       }
       break;
   }
